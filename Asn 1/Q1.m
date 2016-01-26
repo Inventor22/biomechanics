4f3507bbc89a83ecf3be6d7135934952cb601422
Dustin Dobransky <dd2_inventor_climber@hotmail.com>
2016-01-26 13:31:19 -0500

    Added option for extra mass

--- Asn 1/Q1.m
+++ Asn 1/Q1.m
@@ -1,9 +1,9 @@
 
-% weight, height, armAngle,armCOMtoFdeltAngle)
-
-for abductionAngle = [30 60 90 120]
-    disp(['Forces for abduction angle of ' num2str(abductionAngle) ' degrees']);
-    % weight, height, armAngle,armCOMtoFdeltAngle, FdeltDistToJoint
-    ShoulderForces(77, 190, abductionAngle, 7.5, 7.5);
-    disp(' ');
+for massHeld = [0 20]
+    for abductionAngle = [30 60 90 120]
+        disp(['Forces for abduction angle of ' num2str(abductionAngle) ' degrees']);
+        % weight, height, massHeld, armAngle, armCOMtoFdeltAngle, FdeltDistToJoint
+        ShoulderForces(77, 190, massHeld, abductionAngle, 7.5, 7.5);
+        disp(' ');
+    end
 end
